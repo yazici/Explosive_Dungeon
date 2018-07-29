9278a448bc76abccee75a816baae85191eae29ba
vlistop2 <vlistop2@mail.ru>
2018-07-29 13:50:40 +0300

    1.9

---
+++ up/TNT CRASH ULTRA/Assets/ChangeLanguage.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class ChangeLanguage : MonoBehaviour
+{
+    public Text[] Backs;
+    public Text[] Translate;
+    private string json;
+    public static lang lng = new lang();
+    private int langIndex = 1;
+    private string[] langArray = { "ru_RU", "en_US","ua_UA"  };
+
+    void Awake()
+    {
+        if (!PlayerPrefs.HasKey("Language"))
+        {
+            if (Application.systemLanguage == SystemLanguage.Russian || Application.systemLanguage == SystemLanguage.Belarusian)
+                PlayerPrefs.SetString("Language", "ru_RU");
+            else if (Application.systemLanguage == SystemLanguage.Ukrainian) { PlayerPrefs.SetString("Language", "ua_UA"); }
+            else PlayerPrefs.SetString("Language", "en_US");
+        }
+        LangLoad();
+    }
+   /* void Start()
+    {
+        for (int i = 0; i < langArray.Length; i++)
+        {
+            if (PlayerPrefs.GetString("Language") == langArray[i])
+            {
+                langIndex = i + 1;
+                break;
+            }
+        }
+  } */ 
+
+    void LangLoad()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        string path = Path.Combine(Application.streamingAssetsPath, "Language/" + PlayerPrefs.GetString("Language") + ".json");
+        WWW reader = new WWW(path);
+        while (!reader.isDone) { }
+        json = reader.text;
+#else
+        json = File.ReadAllText(Application.streamingAssetsPath + "/Language/" + PlayerPrefs.GetString("Language") + ".json");
+#endif
+        lng = JsonUtility.FromJson<lang>(json);
+    }
+    public void LoadLanguageFull()
+    {
+        for (int i = 0; i != 31; i++) Translate[i].text = ChangeLanguage.lng.Word[i];
+        for (int i = 0; i != Backs.Length; i++) Backs[i].text = ChangeLanguage.lng.Word[16];
+    }
+    public void switchButton()
+    {
+        if (langIndex != langArray.Length) langIndex++;
+       // else langIndex = 1;
+        PlayerPrefs.SetString("Language", langArray[langIndex - 1]);
+        LangLoad();
+        Debug.Log(PlayerPrefs.GetString("Language"));
+        LoadLanguageFull();
+    }
+
+}
+public class lang
+{
+    public string[] Word = new string[31];
+}
--- up/TNT CRASH ULTRA/Assets/MainMenuFunctions.cs
+++ up/TNT CRASH ULTRA/Assets/MainMenuFunctions.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class MainMenuFunctions : MonoBehaviour {
-
+    public Text[] Backs;
+    public Text[] Translate;
     [Header("Канвасы и смена камеры")]
-    public GameObject[] Cameras;
+    public Camera[] Cameras;
+    public Canvas[] Canvases;
 	[Header("Настройки")]
 	public AudioSource MainMenuSource;
 	public AudioSource ButtonSoundSource;
@@ -17,18 +19,25 @@ public class MainMenuFunctions : MonoBehaviour {
     [Header("Статистика")]
     public Text[] Stats;
     public int[] StatsCounts;
-    
-	public void ButtonSound(){if(Sounds){ButtonSoundSource.Play ();}}
+    public void Awake()
+    {
+        LoadLanguageFull();
+    }
+    public void LoadLanguageFull() {
+        for (int i = 0; i != 31; i++) Translate[i].text = ChangeLanguage.lng.Word[i];
+        for (int i = 0; i != Backs.Length; i++) Backs[i].text = ChangeLanguage.lng.Word[16];
+    }
+    public void ButtonSound(){if(Sounds){ButtonSoundSource.Play ();}}
     public void StartButton() {StatsCounts[0]++; PlayerPrefs.SetInt("Games", StatsCounts[0]); Application.LoadLevel(1); }
     public void SetStats() {
-        Stats[0].text = "Games: " + StatsCounts[0];
-        Stats[1].text = "Diamonds collected: " + StatsCounts[1];
-        Stats[2].text = "Best Score: " + StatsCounts[2];
-        Stats[3].text = "Deaths: " + StatsCounts[3];
-        Stats[4].text = "Classic Chests opened: " + StatsCounts[4];
-        Stats[5].text = "Diamond Chest opened: " + StatsCounts[5];
-        Stats[6].text = "Rainbow Chest opened: " + StatsCounts[6];
-        Stats[7].text = "glitch chests opened: " + StatsCounts[7];
+        Stats[0].text = ChangeLanguage.lng.Word[18] + StatsCounts[0];
+        Stats[1].text = ChangeLanguage.lng.Word[19] + StatsCounts[1];
+        Stats[2].text = ChangeLanguage.lng.Word[20] + StatsCounts[2];
+        Stats[3].text = ChangeLanguage.lng.Word[21] + StatsCounts[3];
+        Stats[4].text = ChangeLanguage.lng.Word[22] + StatsCounts[4];
+        Stats[5].text = ChangeLanguage.lng.Word[23] + StatsCounts[5];
+        Stats[6].text = ChangeLanguage.lng.Word[24] + StatsCounts[6];
+        Stats[7].text = ChangeLanguage.lng.Word[25] + StatsCounts[7];
     }
     private void Start()
     {
@@ -37,10 +46,13 @@ public class MainMenuFunctions : MonoBehaviour {
     public void ChangeCamera(int IdToActive){//Метод, меняющий камеру
 		for(int i = 0; i < 5; i++){//Перечисление переменных с массива
 			if (i != IdToActive){//Все переменные массива кроме одной
-				Cameras[i].SetActive(false);
-			}
-		}
-		Cameras [IdToActive].SetActive(true);//Камера активна
+                Cameras[i].enabled = false;
+                Canvases[i].enabled = false;
+            }
+            Cameras[IdToActive].enabled = true;
+            Canvases[IdToActive].enabled = true;
+        }
+        
 	}
 
 	public void TwitterUp(){
@@ -51,25 +63,80 @@ public class MainMenuFunctions : MonoBehaviour {
 		if (Sounds == true) {
 			Sounds = false;
 			ButtonSoundSource.Play ();
-			SoundsText.text = "Sounds: off";
+            switch (PlayerPrefs.GetString("Language"))
+            {
+                case "en_US": SoundsText.text = "Sounds: off"; break;
+                case "ru_RU": SoundsText.text = "Звуки: off"; break;
+                case "ua_UA": SoundsText.text = "Звуки: off"; break;
+            }
 
 		} else {
 			Sounds = true;
 			ButtonSoundSource.Play ();
-			SoundsText.text = "Sounds: on";
-		}
+            switch (PlayerPrefs.GetString("Language"))
+            {
+                case "en_US": SoundsText.text = "Sounds: on"; break;
+                case "ru_RU": SoundsText.text = "Звуки: on"; break;
+                case "ua_UA": SoundsText.text = "Звуки: on"; break;
+            }
+        }
 	}
-
-	public void ChangeMusic (){
+    public void Update()
+    {
+        if (Sounds) {
+            switch (PlayerPrefs.GetString("Language"))
+            {
+                case "en_US": SoundsText.text = "Sounds: on"; break;
+                case "ru_RU": SoundsText.text = "Звуки: on"; break;
+                case "ua_UA": SoundsText.text = "Звуки: on"; break;
+            }
+        } else {
+            switch (PlayerPrefs.GetString("Language"))
+            {
+                case "en_US": SoundsText.text = "Sounds: off"; break;
+                case "ru_RU": SoundsText.text = "Звуки: off"; break;
+                case "ua_UA": SoundsText.text = "Звуки: off"; break;
+            }
+        }
+        if (Music)
+        {
+            switch (PlayerPrefs.GetString("Language"))
+            {
+                case "en_US": MusicText.text = "Music: on"; break;
+                case "ru_RU": MusicText.text = "Музыка: on"; break;
+                case "ua_UA": MusicText.text = "Музика: on"; break;
+            }
+        }
+        else
+        {
+            switch (PlayerPrefs.GetString("Language"))
+            {
+                case "en_US": MusicText.text = "Music: on"; break;
+                case "ru_RU": MusicText.text = "Музыка: on"; break;
+                case "ua_UA": MusicText.text = "Музика: on"; break;
+            }
+        }
+    }
+    public void ChangeMusic (){
 		if (Music == true) {
 			Music = false;
 			MainMenuSource.Stop ();
-			MusicText.text = "Music: off";
+            switch (PlayerPrefs.GetString("Language"))
+            {
+                case "en_US": MusicText.text = "Music: off"; break;
+                case "ru_RU": MusicText.text = "Музыка: off"; break;
+                case "ua_UA": MusicText.text = "Музика: off"; break;
+            }
 
-		} else {
+        } else {
 			Music = true;
 			MainMenuSource.Play ();
-			MusicText.text = "Music: on";
-		}
+            switch (PlayerPrefs.GetString("Language"))
+            {
+                case "en_US": MusicText.text = "Music: on"; break;
+                case "ru_RU": MusicText.text = "Музыка: on"; break;
+                case "ua_UA": MusicText.text = "Музика: on"; break;
+            }
+        }
 	}
 }
