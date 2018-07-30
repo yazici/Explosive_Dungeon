518118446bbd494d53a195781f738bf1a6814fd8
vlistop2 <vlistop2@mail.ru>
2018-07-30 14:28:47 +0300

    1.9.0.2 NihooyaNeSdelal Update

--- up/TNT CRASH ULTRA/Assets/LevelChooseScript.cs
+++ up/TNT CRASH ULTRA/Assets/LevelChooseScript.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 using UnityEngine.UI;
 public class LevelChooseScript : MonoBehaviour {
 
+    private Save sv = new Save();
+    private string path;
     public int curIndex;
-    public bool[] buyedLevels;
     public int[] pricesLevels;
     public Sprite[] BoxState;
     public Image[] LevelsImages;
-    public int Diamonds = 1500;
+    //public int Diamonds = 1500;
     public Text DiamondsText;
     public GameObject[] SecondLevel;
     public GameObject[] ThirdLevel;
@@ -22,25 +25,41 @@ public class LevelChooseScript : MonoBehaviour {
             case 2: Application.LoadLevel(3); break;
         }
     }
+    private void Start()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        path = Path.Combine(Application.persistentDataPath, "Save.json");
+#else
+        path = Path.Combine(Application.dataPath, "Save.json");
+#endif
+        if (File.Exists(path))
+        {
+            sv = JsonUtility.FromJson<Save>(File.ReadAllText(path));
+        }
+    }
     private void LevelBuy(int indexBuy) {
-        buyedLevels[indexBuy] = true;
-        Diamonds = Diamonds - pricesLevels[indexBuy];
+        sv.buyedLevels[indexBuy] = true;
+        sv.Diamonds = sv.Diamonds - pricesLevels[indexBuy];
+    }
+    private void OnApplicationQuit()
+    {
+        File.WriteAllText(path, JsonUtility.ToJson(sv));
     }
     public void Update()
     {
-        DiamondsText.text = "" + Diamonds;
-        if (buyedLevels[1])
+        DiamondsText.text = "" + sv.Diamonds;
+        if (sv.buyedLevels[1])
         {
             SecondLevel[0].SetActive(false);
             SecondLevel[1].SetActive(true);
         }
-        if (buyedLevels[2]) {
+        if (sv.buyedLevels[2]) {
             ThirdLevel[0].SetActive(false);
             ThirdLevel[1].SetActive(true);
         }
     }
     public void OnLevelChecked(int index) {
-        if (buyedLevels[index]) {
+        if (sv.buyedLevels[index]) {
             LevelsImages[index].sprite = BoxState[1];
             curIndex = index;
             for (int i = 0; i < LevelsImages.Length; i++)
@@ -53,7 +72,12 @@ public class LevelChooseScript : MonoBehaviour {
         }
         else
         {
-            if (pricesLevels[index] <= Diamonds) { LevelBuy(index); }
+            if (pricesLevels[index] <= sv.Diamonds) { LevelBuy(index); }
         }
     }
 }
+[Serializable]
+public class Save{
+    public bool[] buyedLevels = { true, false, false };
+    public int Diamonds = 1590;
+   }
